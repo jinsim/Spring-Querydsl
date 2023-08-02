d76cf6794b1fffa2cb95cff4e2cdffc0e1eb3116
jinsim <jinsim726@gmail.com>
2023-08-02 21:02:52 +0900

    프로젝트 생성 및 테스트

--- querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ querydsl/src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -1,13 +1,38 @@
 package study.querydsl;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import study.querydsl.entity.Hello;
+import study.querydsl.entity.QHello;
 
 @SpringBootTest
+@Transactional
 class QuerydslApplicationTests {
+	@Autowired
+	EntityManager em;
 
 	@Test
 	void contextLoads() {
+		Hello hello = new Hello();
+		em.persist(hello);
+
+		// 최근 버전에서는 JPAQueryFactory 를 권장한다.
+		JPAQueryFactory query = new JPAQueryFactory(em);
+		// QHello qHello = new QHello(("h")); // 아래의 코드와 동일
+		QHello qHello = QHello.hello; // Querydsl Q타입 동작 확인
+
+		Hello result = query
+				.selectFrom(qHello) // 쿼리와 관련된 거는 다 Q타입을 넣어야 한다.
+				.fetchOne();
+
+		Assertions.assertThat(result).isEqualTo(hello);
+		// lombok 동작 확인 (hello.getId())
+		Assertions.assertThat(result.getId()).isEqualTo(hello.getId());
 	}
 
 }
